e2ad0c69aed36f964b1a4fa18a2200918f55ffb3
Zakaria Soliman <zakaria.soliman1@gmail.com>
2016-03-06 18:02:36 -0500

    Get multiple JSON puzzle files

--- crossword.js
+++ crossword.js
@@ -3,51 +3,66 @@
  * GLOBAL VARS
  */
 var host = 'http://localhost:3000/';
+var board = [];
+var puzzles = [];
 
 /*
  * Functions
  */
 
 function buildCrosswordTable(puzzle) {
-  var diagram = puzzle.diagram;
   var rows = puzzle.nRows;
   var cols = puzzle.nCols;
   var $crosswordTable = $('#crossword');
   var i = 0;
   var j = 0;
-  var $newRow;
+  var $newRow, id;
 
+  $crosswordTable.empty();
+  board = puzzle.diagram;
   /* Add 1 to rows and cols because we need an extra row and column for the
   numbering */
   for (i = 0; i < rows + 1; i++) {
     $newRow = $('<tr></tr>');
     for (j = 0; j < cols + 1; j++) {
+      id = (i - 1) + '-' + (j - 1);
+
       if (i === 0 && j === 0) {
         $newRow.append('<th></th>');
       } else if (i === 0) { // On the first row we number the columns
-        $newRow.append('<th>' + j.toString() + '</th>');
-      } else if (i > 0 && j === 0) { //On the first column we number the rows
-        $newRow.append('<th>' + i.toString() + '</th>');
-      } else { //Then we just add the boxes
-        $newRow.append('<td contentEditable="true"></td>');
+        $newRow.append('<th>' + j + '</th>');
+      } else if (i > 0 && j === 0) { // On the first column we number the rows
+        $newRow.append('<th>' + i + '</th>');
+      } else { // Then we just add the boxes
+        $newRow.append('<td id="' + id + '" contentEditable="true"></td>');
       }
     }
     $newRow.appendTo($crosswordTable);
   }
 }
 
-function populatePuzzleSelection() {
-  var puzzles = [];
+function getPuzzleFileList() {
+  var files = [];
 
-  // TODO: we need to fetch all the .json files and put them in the puzzles array.
-
-  $.getJSON(host + 'puzzles/IFT3225.json', function(data) {
-    puzzles.push(data);
-    $('select').append('<option value="0">' + data.author + '</option>');
-    buildCrosswordTable(puzzles[0]);
+  $.getJSON(host + 'puzzles/puzzle-list.json', function(data) {
+    files = data.files;
+    populatePuzzleSelection(files);
   });
+}
 
-  return puzzles;
+function populatePuzzleSelection(files) {
+  var url;
+
+  $.each(files, function(i, file) {
+    url = host + 'puzzles/' + file;
+    $.getJSON(url, function(data) {
+      puzzles.push(data);
+      var $select = $('select').append('<option value="' + i + '">' + file.split('.')[0] + '</option>');
+      if (i === 0) {
+        buildCrosswordTable(puzzles[$select.val()]);
+      }
+    });
+  });
 }
 
 function keyPressed() {
@@ -59,5 +74,8 @@ function keyPressed() {
  */
 
 $(document).ready(function() {
-  populatePuzzleSelection();
+  getPuzzleFileList();
+  $('select').change(function(event) {
+    buildCrosswordTable(puzzles[event.target.value]);
+  });
 });

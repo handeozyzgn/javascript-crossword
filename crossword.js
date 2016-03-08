f9f164c311a68455169e79b02ab498aaa3881000
Zakaria Soliman <zakaria.soliman1@gmail.com>
2016-03-08 16:18:32 -0500

    Some changes to keep eslint somewhat happy

--- crossword.js
+++ crossword.js
@@ -10,19 +10,6 @@ var selectedPuzzle;
 /*
  * Functions
  */
-function initCrossword() {
-  $('select').change(function(event) {
-    selectedPuzzle = puzzles[event.target.value];
-    buildCrosswordTable(selectedPuzzle);
-  });
-
-  var files = [];
-  // Fetch all the json files from our server
-  $.getJSON(host + 'puzzles/puzzle-list.json', function(data) {
-    files = data.files;
-    populatePuzzleSelection(files);
-  });
-}
 
 function buildCrosswordTable(puzzle) {
   var rows = puzzle.nRows;
@@ -30,7 +17,7 @@ function buildCrosswordTable(puzzle) {
   var $crosswordTable = $('#crossword');
   var i = 0;
   var j = 0;
-  var $newRow, id;
+  var $newRow, $td, elem, id;
 
   $crosswordTable.empty();
   board = puzzle.diagram;
@@ -48,10 +35,11 @@ function buildCrosswordTable(puzzle) {
       } else if (i > 0 && j === 0) { // On the first column we number the rows
         $newRow.append('<th>' + i + '</th>');
       } else { // Then we just add the crossword puzzle boxes
-        var $td = $('<td id="' + id + '" class="' + 'row-' + (i - 1) + ' col-' + (j - 1) + '" contentEditable="true"></td>').appendTo($newRow);
-        if (board[i - 1][j - 1] === ".") {
+        elem = '<td id="' + id + '" class="' + 'row-' + (i - 1) + ' col-' + (j - 1) + '" contentEditable="true"></td>';
+        $td = $(elem).appendTo($newRow);
+        if (board[i - 1][j - 1] === '.') {
           $td.addClass('blacked-out');
-          $td.attr('contentEditable', 'false')
+          $td.attr('contentEditable', 'false');
         }
       }
     }
@@ -59,33 +47,18 @@ function buildCrosswordTable(puzzle) {
   }
 }
 
-function populatePuzzleSelection(files) {
-  var url;
-
-  $.each(files, function(i, file) {
-    url = host + 'puzzles/' + file;
-    $.getJSON(url, function(data) {
-      puzzles.push(data);
-      var $select = $('select').append('<option value="' + i + '">' + file.split('.')[0] + '</option>');
-      if (i === 0) {
-        selectedPuzzle = puzzles[$select.val()];
-        buildCrosswordTable(selectedPuzzle);
-        populateClues();
-      }
-    });
-  });
-}
-
 function populateClues() {
   var tableHeight = $('#crossword').height();
-  $('#horizontal').css('height', tableHeight);
-  $('#vertical').css('height', tableHeight);
   var $horClueList = $('#horizontal-clues ul');
   var $vertClueList = $('#vertical-clues ul');
+  var clue, i, j;
 
-  for (var i = 0; i < selectedPuzzle.nRows; i++) {
-    for (var j = 0; j < selectedPuzzle.nCols; j++) {
-      var clue = selectedPuzzle.numbers[i][j];
+  $('#horizontal').css('height', tableHeight);
+  $('#vertical').css('height', tableHeight);
+
+  for (i = 0; i < selectedPuzzle.nRows; i++) {
+    for (j = 0; j < selectedPuzzle.nCols; j++) {
+      clue = selectedPuzzle.numbers[i][j];
 
       if (selectedPuzzle.acrossClues[clue]) {
         $horClueList.append('<li>' + selectedPuzzle.acrossClues[clue] + '</li>');
@@ -95,12 +68,42 @@ function populateClues() {
       }
     }
   }
+}
 
+function populatePuzzleSelection(files) {
+  var url, $select;
 
+  $.each(files, function(i, file) {
+    url = host + 'puzzles/' + file;
+    $.getJSON(url, function(data) {
+      puzzles.push(data);
+      $select = $('select').append('<option value="' + i + '">' + file.split('.')[0] + '</option>');
+      if (i === 0) {
+        selectedPuzzle = puzzles[$select.val()];
+        buildCrosswordTable(selectedPuzzle);
+        populateClues();
+      }
+    });
+  });
 }
 
 function keyPressed() {
   // TODO: Handle the interactions. Helper functions will probably be needed
+}
+
+function initCrossword() {
+  var files = [];
+
+  $('select').change(function(event) {
+    selectedPuzzle = puzzles[event.target.value];
+    buildCrosswordTable(selectedPuzzle);
+  });
+
+  // Fetch all the json files from our server
+  $.getJSON(host + 'puzzles/puzzle-list.json', function(data) {
+    files = data.files;
+    populatePuzzleSelection(files);
+  });
 }
 
 /*
